095fb08ee753792c354ea0080c0849bb36ebcb37
Shuhrat <abbosovich1999@gmail.com>
2025-06-21 21:26:49 +0500

    vazifa yami

---
+++ Vazifalar/homework2/math.js
@@ -0,0 +1,22 @@
+const pow = (num,dere)=>{
+    return num**dere
+}
+
+const add = (a,b)=> {
+
+    return a+b
+}
+
+const ayrish = (a,b)=>{
+    return a-b
+}
+
+// export {
+//     add,ayrish
+// }
+
+
+
+module.exports ={
+    add,ayrish
+}
---
+++ Vazifalar/homework2/script.js
@@ -0,0 +1,17 @@
+
+
+// esm modulda
+
+// import { add,ayrish } from "./math.js";
+
+// console.log(add(44,66));
+// console.log(ayrish(88,56));
+
+
+
+
+const { add, ayrish }= require('./math.js')
+
+console.log(add(45,55));
+
+console.log(ayrish(55,12));

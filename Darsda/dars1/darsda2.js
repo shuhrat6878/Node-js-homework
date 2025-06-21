42171b59225b44db5a191e5700be9901cd068010
Shuhrat <abbosovich1999@gmail.com>
2025-06-21 11:41:49 +0500

    new

---
+++ Darsda/dars1/darsda2.js
@@ -0,0 +1,13 @@
+let tex = "salom" 
+
+const fs =  require('fs');
+
+const data =fs.statSync('100 mb-text.text');
+
+const mb =1024*1024*512;
+
+const bigFilePath ='big-file.txt';
+
+fs.appendFileSync('gib-file.txt');
+
+console.log(Stats.size)

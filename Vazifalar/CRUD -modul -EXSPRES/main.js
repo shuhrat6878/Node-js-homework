88adf8f40998092dbbd13364ede0fd5d01e5cc2f
Shuhrat <abbosovich1999@gmail.com>
2025-06-29 23:34:31 +0500

    vazifa bajarildi

--- Vazifalar/CRUD -modul -EXSPRES/main.js
+++ Vazifalar/CRUD -modul -EXSPRES/main.js
@@ -1,14 +1,109 @@
-import express from 'express'; 
-import { read,write } from './write-read.js';
-
+import express from 'express';
+import { read, write } from './write-read.js';
 
 const app = express();
 
-
 app.use(express.json());
-app.listen(3000, ()=> {
-    console.log("server 3000 porta ishlayapti")})
 
-app.get ('/', (req,res)=>{
-    res.send("salom dunyo")
+app.post('/', async (req, res) => {
+  try {
+    const users = await read();
+    const newUser = {
+      id: !users.length ? 1 : users.at(-1).id + 1,
+      ...req.body
+    };
+    users.push(newUser);
+    await write(users);
+    return res.status(200).json({
+      data: newUser
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || "xatolik yuz berdi"
+    });
+  }
 });
+
+app.get ('/', async(req,res)=>{
+    try {
+        const users = await read();
+        return res.status(201).json({
+            data : users
+        });
+        
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message || "xatolik yuz berdi"
+        });
+    }
+});
+
+app.get ('/:id', async (req,res)=>{
+    try {
+        const id = +req.params.id;
+        const users  = await read();
+        const user = users.find ((user)=> user.id === id);
+        if (!user){
+            return res.status(404).json({
+                message: "foydalanuvshi topilmadi"
+            });
+        }
+        return res.status(200).json({
+            data: user
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message || "xatolik yuz berdi"
+        });
+        
+    }
+});
+app.put('/:id', async (req, res) => {
+  try {
+    const id = +req.params.id;
+    const users = await read();
+    const userIndex = users.findIndex((user) => user.id === id);
+    if (userIndex === -1) {
+      return res.status(404).json({
+        message: "foydalanuvchi topilmadi"
+      });
+    }
+    users[userIndex] = { id, ...req.body };
+    await write(users);
+    return res.status(200).json({
+      data: users[userIndex]
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || "xatolik yuz berdi"
+    });
+  }
+});
+
+app.delete('/:id', async (req, res) => {
+  try {
+    const id = +req.params.id;
+    const users = await read();
+    const userIndex = users.findIndex((user) => user.id === id);
+    if (userIndex === -1) {
+      return res.status(404).json({
+        message: "foydalanuvchi topilmadi"
+      });
+    }
+    users.splice(userIndex, 1);
+    await write(users);
+    return res.status(200).json({
+      message: "foydalanuvchi o'chirildi"
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || "xatolik yuz berdi"
+    });
+    }
+});
+
+app.listen(3000, () => {
+  console.log("Server 3000 portda ishlayapti");
+});
+
+
--- Vazifalar/CRUD -modul -EXSPRES/write-read.js
+++ Vazifalar/CRUD -modul -EXSPRES/write-read.js
@@ -1,16 +1,16 @@
 import { writeFile, readFile } from 'node:fs/promises';
-import { join } from 'none:path';
+import { join } from 'node:path';
 
 const filePath = join('./users.json');
 
 export const write = async (data) => {
-    await writeFile(filePath, JSON.stringfiy(data, null, 2))
+    await writeFile(filePath, JSON.stringify(data, null, 2))
 
 }
 
 
 export const read  =async( )=>{
      const data= await readFile(filePath,'utf8')
-     return JSON.parse (data)
+     return JSON.parse(data)
 
 }
